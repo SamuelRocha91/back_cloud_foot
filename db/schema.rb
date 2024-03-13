c651bb771ade7bf7c99691255a7cea5fd3a5b58e
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-03-13 18:37:50 -0300

    feat: implementa criptografia de senha

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_13_203905) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_13_213354) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
