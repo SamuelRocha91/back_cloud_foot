cc1659a10a1bc740320f68f969bd192e0daeffb3
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-03-13 16:53:39 -0300

    feat: adiciona bullet, rspec, faker e shoulda matchers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,14 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  Rails.application.configure do
+  # Outras configurações...
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.bullet_logger = true
+    Bullet.rails_logger = true
+  end
+  end
 end
